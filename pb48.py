ab4090ebef8bbda9b3fb9fdcc057b3ef7ffacfa7
ZHANG Chenyu <zcy.scott@gmail.com>
2017-02-15 10:27:00 +0100

    add pb48

---
+++ pb48.py
@@ -0,0 +1,15 @@
+#coding: utf
+import time
+
+def solve():
+    sum = 0
+    for i in xrange(1, 1001):
+        print i
+        sum += i**i
+    return str(sum)[-10:]
+    
+if __name__ == '__main__':
+    start = time.time()
+    print "The answer is {}".format(solve())
+    end = time.time()
+    print "it took {} seconds".format(end - start)
